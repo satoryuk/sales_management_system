d6f942b85aee0ac795849947d6b3d72257916726
satoryuk <your_email@example.comthong.vathana.2822@rupp.edu.kh>
2025-03-17 00:05:06 +0700

    new update

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,15 @@
+// export default function Home() {
+//   return (
+//     <div className="flex items-center justify-center min-h-screen bg-gray-100">
+//       <h1 className="text-5xl font-bold text-red-500">Home</h1>
+//     </div>
+//   );
+// }
+
 export default function Home() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-5xl font-bold text-red-500">Home</h1>
+    <div>
+      <h1>Home</h1>
     </div>
   );
 }
